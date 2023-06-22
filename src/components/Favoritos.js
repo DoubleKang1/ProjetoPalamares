91070e42ae11e32f6c6e804f42d8485d6f326bc3
PCCAMARASC SaoCarlos <rafaeldalves@outlook.com>
2023-06-22 13:32:32 -0300

    Adicionando o icone de favoritos

--- src/components/Favoritos.js
+++ src/components/Favoritos.js
@@ -1,5 +1,11 @@
-const Favoritos = () => {
+import './Favoritos.css'
 
+const Favoritos = () => {
+    return (
+        <div className="padding">
+            <h1>Favoritos</h1>
+        </div>
+    )
 }
 
 export default Favoritos;
--- src/components/HomePost.js
+++ src/components/HomePost.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { FaHeart } from 'react-icons/fa';
 import './HomePost.css';
+import { useState } from 'react';
 
 const HomePost = (props) => {
 
+
+    const [isLiked, setIsLiked] = useState(false);
+
+    // Função para alternar o estado do coração
+    const toggleLike = () => {
+        setIsLiked(!isLiked);
+    }
 
     return (
         <div>
@@ -12,6 +21,10 @@ const HomePost = (props) => {
                         <div className='center'>
                             <div className="card">
                                 <h2>{post.name}</h2>
+                                <FaHeart
+                                    className={`heart-icon ${isLiked ? 'liked' : ''}`}
+                                    onClick={toggleLike}
+                                />
                                 <img src={post.imageUrl} alt="Imagem da postagem" className="post-image" />
                                 <h3 className="subtitle">{post.date} | {post.dateDeath} </h3>
                                 <p className="bio">{post.bio}</p>
