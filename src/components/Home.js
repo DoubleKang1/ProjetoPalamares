95778ea8751e56a32c220e09bd289df5d849b56a
PCCAMARASC SaoCarlos <rafaeldalves@outlook.com>
2023-06-23 11:36:59 -0300

    Adicionando css do cadastro de posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,35 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './Home.css';
 import { PostContext } from '../folderContext/PostsProvider';
 import HomePost from '../components/HomePost'
 const Home = () => {
 
     const postsCtx = useContext(PostContext);
-    console.log(postsCtx.posts);
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleForm = () => {
+        setIsExpanded((prevState) => !prevState);
+    };
 
     return (
         <div>
-            <form onSubmit={postsCtx.onPostSubmit}>
-                <div className="linha-horizontal">
-                    <input type="text" name="name" placeholder='Nome do homenageado' />
-                    <input type="text" name="imageUrl" placeholder='Link da foto' />
-                    <input type="date" name="date" placeholder='Data de nascimento' />
-                    <input type="date" name="dateDeath" placeholder='Data de falecimento' />
-                </div>
-                <div className='textArea'>
-                    <textarea rows="5" cols="60" name="bio" placeholder='Biografia'>asasa</textarea>
-                    <input type="submit" value="Cadastrar" />
-                </div>
-            </form>
+            <span className='center'>
+                <button onClick={toggleForm}>Cadastrar post</button></span>
+            {isExpanded && (
+                <form onSubmit={postsCtx.onPostSubmit}>
+                    <div className="linha-horizontal">
+                        <input type="text" name="name" placeholder='Nome do homenageado' />
+                        <input type="text" name="imageUrl" placeholder='Link da foto' />
+                        <input type="date" name="date" placeholder='Data de nascimento' />
+                        <input type="date" name="dateDeath" placeholder='Data de falecimento' />
+                    </div>
+                    <div className='textArea'>
+                        <textarea rows="5" cols="60" name="bio" placeholder='Biografia'>asasa</textarea>
+                        <input type="submit" value="Cadastrar" />
+                    </div>
+                </form>
+            )}
+
             <center><h1>Home</h1></center>
             <div>
                 {<HomePost posts={postsCtx.posts} />}
